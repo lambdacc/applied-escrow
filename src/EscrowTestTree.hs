964723cf60c94afa4e6aae34f48afdda547191af
Ganesh <ganesh.nithyanandam@gmail.com>
2021-10-25 07:44:14 +0530

    Tests

--- src/EscrowTestTree.hs
+++ src/EscrowTestTree.hs
@@ -12,7 +12,7 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 
-module EscrowTest where
+module EscrowTestTree where
 
 import           AppliedEscrow
 import           Control.Monad              hiding (fmap)
@@ -32,8 +32,11 @@ import           Test.Tasty
 import           Wallet.Emulator.Wallet
 
 
-test :: IO ()
-test = runEmulatorTraceIO $ runTrace1
+mainTest :: IO ()
+mainTest = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [publishAndAcceptTest,publishAcceptAndPayTranchesTest]
 
 w1, w2, w3 :: Wallet
 w1 = X.knownWallet 1
@@ -54,6 +57,13 @@ testContractAmount = 5_000_000
 
 testTrancheCount :: Integer
 testTrancheCount = 4
+
+publishAndAcceptTest :: TestTree
+publishAndAcceptTest = checkPredicate "Publish and accept test"
+    ( walletFundsChange w1 mempty
+      .&&. walletFundsChange w2 (Ada.lovelaceValueOf $ negate testContractAmount)
+    )
+    $ runTrace1
 
 runTrace1 ::EmulatorTrace ()
 runTrace1 = do
@@ -91,5 +101,51 @@ runTrace1 = do
                     }
 
     callEndpoint @"accept" h2 useParam
+    void $ Emulator.waitNSlots 1
 
+
+--Lifecycle stage 1,2 tests
+publishAcceptAndPayTranchesTest :: TestTree
+publishAcceptAndPayTranchesTest = checkPredicate "Publish, accept and pay tranches tests"
+    ( walletFundsChange w1 (Ada.lovelaceValueOf $ testContractAmount)
+      .&&. walletFundsChange w2 (Ada.lovelaceValueOf $ negate testContractAmount)
+    )
+    $ runTrace2
+
+runTrace2 ::EmulatorTrace ()
+runTrace2 = do
+    h1 <- activateContractWallet w1 startEscrowEndpoint
+    h2 <- activateContractWallet w2 acceptEscrowEndpoint
+
+    let pkh1      = (pubKeyHash . walletPubKey) $ w1
+        pkh2      = (pubKeyHash . walletPubKey) $ w2
+        amount    = testContractAmount
+        startTime = slotToBeginPOSIXTime def 10
+        endTime   = slotToBeginPOSIXTime def 15
+
+        param = PublishParam
+                { p      = pkh1
+                , c      = pkh2
+                , st     = startTime
+                , et     = endTime
+                , tc     = testTrancheCount
+                , ll     = amount
+                }
+
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"publish" h1 (param, True)
+    tt <- getTT h1
+    void $ Emulator.waitNSlots 1
+
+    let useParam = UseParam
+              { up      = pkh1
+              , uc      = pkh2
+              , ust     = startTime
+              , uet     = endTime
+              , ull     = amount
+              , utc     = testTrancheCount
+              , uttn    = tt
+              }
+
+    callEndpoint @"accept" h2 useParam
     void $ Emulator.waitNSlots 1
